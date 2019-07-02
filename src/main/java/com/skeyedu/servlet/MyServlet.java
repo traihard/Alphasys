0777cda1744477e4daf34bce54ace322a6570526
traihard <646887219@qq.com>
2019-07-02 16:20:33 +0800

    Alpha 0.0.1

---
+++ src/main/java/com/skeyedu/servlet/MyServlet.java
@@ -0,0 +1,39 @@
+package com.skeyedu.servlet;
+
+import javax.servlet.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class MyServlet implements Servlet {
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        PrintWriter out = res.getWriter();
+        out.println("<html>");//输出的内容要放在HTML中
+        out.println("<body>");
+        out.println("qwerty123");
+        out.println("</body>");
+        out.println("</html>");
+
+
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
